91025165f1b963d1b49f94cfb5aff492312fad69
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-02-16 18:04:15 +0000

    Added 101-quote.c program

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,13 @@
 #include <unistd.h>
+
+/**
+ * main - Prints a quote to standard error
+ *
+ * Description: This program prints the exact quote:
+ * "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error output.
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);

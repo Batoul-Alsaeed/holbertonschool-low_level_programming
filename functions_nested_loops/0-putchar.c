f1e9e093239b202341ca36937cc08743f084e51f
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-02-18 14:47:43 +0000

    Updated 0-putchar.c and main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,7 +10,7 @@ int main(void)
 	char *str = "_putchar";
 	int i= 0;
 
-	while (str[i]! = '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
@@ -18,4 +18,4 @@ int main(void)
 	_putchar ('\n');
 
 	return (0);
-i}
+}

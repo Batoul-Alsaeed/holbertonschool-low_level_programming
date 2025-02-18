c813e9dd45f07ca12ac2d122c928e5eb4eb20bde
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-02-18 15:20:43 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet -(a-z)
+ *
+ * Return: (vod)
+ */
+void print_alphabet(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+
+	for (i = 0; alphabet[i] != '\0'; i++)
+	{
+	 _putchar(alphabet[i]);
+	}
+}

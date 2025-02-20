3bba36c0fdbd4658cd8fe2c6e5e8a85ad204d302
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-02-20 18:35:16 +0000

    104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers
- * Return: Always 0 (Success)
+ * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	unsigned long a = 1, b = 2, temp;
-	unsigned long a_high, a_low, b_high, b_low;
-	unsigned long temp_high, temp_low;
 	int count;
+	unsigned long fib1_lower = 1, fib1_upper = 0;
+	unsigned long fib2_lower = 2, fib2_upper = 0;
+	unsigned long fib_lower, fib_upper, carry;
 
-	printf("%lu, %lu", a, b);
+	printf("%lu, %lu", fib1_lower, fib2_lower);
 
-	for (count = 3; count <= 92; count++)
+	for (count = 3; count <= 98; count++)
 	{
-	temp = a + b;
-	printf(", %lu", temp);
-	a = b;
-	b = temp;
-	}
+		fib_lower = fib1_lower + fib2_lower;
+		fib_upper = fib1_upper + fib2_upper;
 
-	a_high = a / 1000000000;
-	a_low = a % 1000000000;
-	b_high = b / 1000000000;
-	b_low = b % 1000000000;
+		if (fib_lower >= 1000000000)
+		{
+			carry = fib_lower / 1000000000;
+			fib_lower %= 1000000000;
+			fib_upper += carry;
+		}
 
-	for (; count <= 98; count++)
-	{
-	temp_high = a_high + b_high;
-	temp_low = a_low + b_low;
+		if (fib_upper > 0)
+			printf(", %lu%09lu", fib_upper, fib_lower);
+		else
+			printf(", %lu", fib_lower);
 
-	if (temp_low > 999999999)
-	{
-		temp_high += 1;
-		temp_low %= 1000000000;
-	}
-
-	printf(", %lu%09lu", temp_high, temp_low);
-
-	a_high = b_high;
-	a_low = b_low;
-	b_high = temp_high;
-	b_low = temp_low;
+		fib1_upper = fib2_upper;
+		fib1_lower = fib2_lower;
+		fib2_upper = fib_upper;
+		fib2_lower = fib_lower;
 	}
 
 	printf("\n");

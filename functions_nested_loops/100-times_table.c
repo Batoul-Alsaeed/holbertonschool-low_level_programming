2e095b1f5a35bb46f6f5cbfb5ad13534b5f46fa9
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-02-20 18:08:06 +0000

    Fixed print_times_table to use _putchar instead of printf

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_times_table - Prints the n times table, starting with 0
@@ -18,11 +17,33 @@ void print_times_table(int n)
 		{
 			result = row * col;
 
-			if (col == 0)
-				printf("%d", result);
-			else
-				printf(", %3d", result);
+			if (col != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (result < 100)
+				{
+					_putchar(' ');
+				}
+			}
+
+			if (result >= 100)
+			{
+				_putchar((result / 100) + '0');
+				_putchar(((result / 10) % 10) + '0');
+			}
+			else if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+			}
+			_putchar((result % 10) + '0');
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }

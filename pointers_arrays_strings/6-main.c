b4bc296290d4947ed0c7e6cf2e79c653f12d979f
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-02-26 13:48:35 +0000

    Add 6-puts2.c

--- pointers_arrays_strings/6-main.c
+++ pointers_arrays_strings/6-main.c
@@ -7,9 +7,8 @@
  */
 int main(void)
 {
-    char *str;
-
-    str = "0123456789";
-    puts2(str);
-    return (0);
+	char *str= "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+	
+	puts2(str);
+	return (0);
 }

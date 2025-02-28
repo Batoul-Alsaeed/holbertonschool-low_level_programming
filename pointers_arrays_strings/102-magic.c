ee1f263de6049e9d38105de79584bf906320e47a
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-02-28 14:52:07 +0000

    102-magic.c

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -14,6 +14,6 @@ int main(void)
 * - only one statement. you are not allowed to code anything else than this line of code
 */
 	*(p + 5) = 98;
-	printf("a[2] = %d\n", a[2]);
+	printf("a[2] = %d\n", *(p + 5));
 	return (0);
 }

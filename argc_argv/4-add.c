c3666b15877e6c11c5a506bf57341a282cec2ec9
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-03-06 01:43:44 +0000

    Task 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,15 +10,15 @@
  */
 int is_number(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i])
-    {
-        if (!isdigit(str[i]))
-            return (0);
-        i++;
-    }
-    return (1);
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
@@ -30,24 +30,24 @@ int is_number(char *str)
  */
 int main(int argc, char *argv[])
 {
-    int i, sum = 0;
+	int i, sum = 0;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        if (!is_number(argv[i]))
-        {
-            printf("Error\n");
-            return (1);
-        }
-        sum += atoi(argv[i]);
-    }
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }

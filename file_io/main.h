21c3b2bc77d562138f3acfe4a06f71fe408c723c
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-04-05 20:13:53 +0000

    .

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,11 @@
 
 #include <unistd.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
